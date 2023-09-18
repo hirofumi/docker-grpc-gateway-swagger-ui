ed0ac928af27f88814c49598bcbe38c44c52a0e8
Hirofumi Yoshikawa <hirofumi@users.noreply.github.com>
2023-09-18 16:44:06 +0900

    Use puppeteer for test

---
+++ test/test.js
@@ -0,0 +1,22 @@
+const assert = require('node:assert');
+const puppeteer = require('puppeteer');
+
+const URL = 'http://grpc-gateway-swagger-ui:3000/';
+
+(async () => {
+    const browser = await puppeteer.launch({ headless: 'new' });
+    const page = await browser.newPage();
+
+    await page.goto(URL);
+    await page.waitForSelector('.swagger-ui');
+    assert.equal(
+        page.url(),
+        URL + '?url=/api/apidocs.swagger.json',
+    );
+    assert.equal(
+        await page.evaluate(() => document.querySelector('h2.title').innerText),
+        'example.proto\n version not set ',
+    );
+
+    await browser.close();
+})();
